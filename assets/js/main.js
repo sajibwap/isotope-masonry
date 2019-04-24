dd94206f0b35790c6a71cff4b25a13d7c8994133
Sajjad Hossain Sajib <sajibwap@gmail.com>
2019-04-24 23:59:43 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function($){
+
+		$('.project-title li').on('click', function(){
+
+			$('.project-title li').removeClass('active');
+			$(this).addClass('active');
+
+			var selector = $(this).attr('data-filter');
+			$(".project-list").isotope({
+				filter: selector
+			});
+		});
+
+		jQuery('.project-list').isotope();
+
+
+	});
+
+
+
+}(jQuery));	
